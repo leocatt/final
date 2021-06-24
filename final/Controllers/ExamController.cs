a75d891ff4b7a78af5bd5d38965d7e90dd720234
華哲宇 <h2577750395@gmail.com>
2021-06-24 19:34:05 +0800

    exam

---
+++ final/Controllers/ExamController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+        public ActionResult Week(float? week)
+        {
+           
+            var result = "";
+            if (week.HasValue)
+            {
+                if (week == 1) result = "星期一";
+                if (week == 2) result = "星期二";
+                if (week == 3) result = "星期三";
+                if (week == 4) result = "星期四";
+                if (week == 5) result = "星期五";
+                if (week == 6) result = "星期六";
+                if (week == 7) result = "星期七";
+            }
+            
+            ViewBag.Result = result;
+
+
+            return View();
+        }
+
+    }
+}
